2b21b78b8326f315ee902b997f9dd73c8060852e
KIM <m.ceda@hcdc.edu.ph>
2022-02-21 17:22:42 +0800

    GUI: PRODUCER-CLEARED RETAILER-TBF LOGISTICS-TBF
    CODES:
    TBF
    
    baka maguba ang app

---
+++ app/src/main/java/com/example/test2/producer_main_orders.java
@@ -0,0 +1,14 @@
+package com.example.test2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class producer_main_orders extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.producer_main_orders);
+    }
+}
---
+++ app/src/main/java/com/example/test2/producer_product_details.java
@@ -0,0 +1,14 @@
+package com.example.test2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class producer_product_details extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.producer_product_details);
+    }
+}
